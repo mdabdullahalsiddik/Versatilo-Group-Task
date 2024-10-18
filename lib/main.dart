09e3d603861ceb625f6c033fc78d7ea2caf458dd
NS NooR <non377938@gmail.com>
2024-10-17 15:43:21 -1200

    main dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
-import 'package:test_1/view/screens/home/home.dart';
+import 'package:test_1/view/screens/splash/splash.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home: Splash(),
     );
   }
 }
