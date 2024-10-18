719f674f5b4abd6405b1b81d54d1844b939e683b
mabdullahalsiddik <mabdullahalsiddik@gmail.com>
2024-10-18 23:47:07 +0600

    fix

--- lib/view/screens/photo/edit.dart
+++ lib/view/screens/photo/edit.dart
@@ -2,18 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:test_1/controller/ui/photo_edit.dart';
 
-
-
 class PhotoEditorScreen extends StatelessWidget {
-  const PhotoEditorScreen({super.key,});
- 
+  const PhotoEditorScreen({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
     final PhotoEditController controller = Get.put(PhotoEditController());
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Photo Editor'),
         actions: [
           IconButton(
             icon: const Icon(Icons.crop),
@@ -41,8 +39,7 @@ class PhotoEditorScreen extends StatelessWidget {
           return Center(
             child: ElevatedButton(
               onPressed: () {
-                controller
-                    .loadSampleImage(); 
+                controller.loadSampleImage();
               },
               child: const Text('Load Sample Image'),
             ),
